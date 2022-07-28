c558271d755f4b14e2ac024927dc4a1798758e42
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-28 11:08:22 +0300

    Initial Commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by Ömer Faruk Kılıçaslan on 28.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
